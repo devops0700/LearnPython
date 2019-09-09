15692a8729ed8636f1573e8b8d01cd5e38c0c6a7
devops0700 <devops.0700@gmail.com>
2019-09-09 23:02:25 +1000

    added os_env file

---
+++ os_env.py
@@ -0,0 +1,10 @@
+import time
+import os
+input1 = os.getenv("input1")
+input2 = os.getenv("input2")
+Dropdown = os.getenv("Dropdown1")
+Dropdown2 = os.getenv("Dropdown2")
+print input1
+print input2
+print Dropdown
+print Dropdown2
