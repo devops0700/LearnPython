318f967c3493e8566f10c27e9224e9a09e6ed86e
devops0700 <devops.0700@gmail.com>
2019-10-08 01:04:23 +1100

    created orcl_db_conn script

--- inp_argv.py
+++ inp_argv.py
@@ -4,4 +4,4 @@ Name = argv[1]
 Age = argv[2]
 Nationality = argv[3]
 
-print (Name + " " + str(Age) + " " + Nationality)
+print ("My name is: " + Name + " and My age is: " + str(Age) + " and I am an " + Nationality + " citizen.")
