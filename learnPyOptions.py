0f9f63deea76e57add1459aaf6980bdc0f77a2b0
devops0700 <devops.0700@gmail.com>
2019-10-21 22:50:44 +1100

    added parse_json01 script

---
+++ learnPyOptions.py
@@ -0,0 +1,8 @@
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+# print(cars[0])
+for car in cars:
+    print(car)
+    if car == "Volvo":
+        break
+#    print(car)
--- orcl_db_conn.py
+++ orcl_db_conn.py
@@ -1,4 +1,4 @@
-import cx_Oracle
+import cx_oracle
 
 con = cx_Oracle.connect('pythonhol/welcome@127.0.0.1/orcl')
 print con.version
